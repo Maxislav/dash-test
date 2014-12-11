712e742ea06c1d79fc09a1333a4441c6b8814c6c
mars <lmaxim@mail.ru>
2014-12-11 18:51:22 +0200

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function (grunt) {
+
+	var sassFiles = [
+		'css/default.scss'
+	]
+
+
+	grunt.initConfig({
+		sass: {
+			dev: {
+				options: {
+					sourcemap: 'auto',
+					style : 'compressed'
+				},
+				files: {
+					'build/default.css': sassFiles
+				}
+			},
+			prod: {
+				options: {
+					sourcemap: 'none'
+				},
+				files: {
+					'build/default.css': sassFiles
+				}
+			}
+		},
+		watch: {
+			_sass: {
+				files: sassFiles,
+				tasks: ['sass:dev'],
+				options: {
+					nospawn: true
+				}
+			}
+		}
+	});
+
+
+
+
+
+	grunt.loadNpmTasks('grunt-contrib-uglify');//
+	grunt.loadNpmTasks('grunt-contrib-watch');//
+	grunt.loadNpmTasks('grunt-string-replace');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+
+	grunt.registerTask('default', ['sass:dev', 'watch']);
+}
